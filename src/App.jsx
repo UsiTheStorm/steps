43cd16a4dcdc616f9d26ef7335cea30c38e2ea06
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-15 11:28:20 +0100

    Add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
+
 function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">{messages[0]}</p>
       <div className="buttons">
         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Previous</button>
         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Next</button>
