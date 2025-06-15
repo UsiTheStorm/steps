6724db911791eca2d444502711511aae44424b2c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-15 13:18:10 +0100

    Add open/close state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
   }
@@ -13,25 +15,36 @@ function App() {
     if (step < 3) setStep(step + 1);
   }
 
+  function handleClose() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={{ color: '#fff' }} onClick={handlePrevious} disabled={step === 1}>
-          Previous
-        </button>
-        <button style={{ color: '#fff' }} onClick={handleNext} disabled={step === 3}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={handleClose}>
+        {isOpen ? '×' : '+'}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button style={{ color: '#fff' }} onClick={handlePrevious} disabled={step === 1}>
+              Previous
+            </button>
+            <button style={{ color: '#fff' }} onClick={handleNext} disabled={step === 3}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default App;
